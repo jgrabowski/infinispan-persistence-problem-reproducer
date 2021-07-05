ce994b56a870b80c3decbb0f98cf814e027cf9b8
Jakub Grabowski <jgrabowski@openbs.pl>
2021-07-05 14:59:42 +0200

    first commit

---
+++ src/test/java/pl/perfectsource/quarkus/bug/persistence/NativeNotificationResourceIT.java
@@ -0,0 +1,9 @@
+package pl.perfectsource.quarkus.bug.persistence;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeNotificationResourceIT extends NotificationResourceTest {
+
+    // Execute the same tests but in native mode.
+}
