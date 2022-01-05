48dd8070daeb36437c05968f874992b0cfe5f9de
Jakub Grabowski <jgrabowski@openbs.pl>
2022-01-05 14:32:22 +0100

    Kogito update to 1.15.0-SNAPSHOT

--- src/test/java/pl/perfectsource/quarkus/bug/persistence/NativeNotificationResourceIT.java
+++
@@ -1,9 +0,0 @@
-package pl.perfectsource.quarkus.bug.persistence;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeNotificationResourceIT extends NotificationResourceTest {
-
-    // Execute the same tests but in native mode.
-}
